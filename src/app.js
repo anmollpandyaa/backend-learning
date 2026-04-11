b90b8c3087a450204b510e20aa0d05a0abf2d2fd
anmollpandyaa <anmolpandya75@gmail.com>
2026-04-11 22:19:18 +0530

    added limit constant

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import { LIMIT } from "./constants"
 
 const app = express()
 
 app.use(cors({origin: process.env.CORS_ORIGIN}))
-app.use(express.json({limit: "10kb"}))
-app.use(express.urlencoded({extended: true, limit: "10kb"}))
+app.use(express.json({limit: LIMIT}))
+app.use(express.urlencoded({extended: true, limit: LIMIT}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "learningdb"
+export const LIMIT = "10kb"
