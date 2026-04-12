b705bf80785c83b9dc84dc614a2a04da39faa0de
anmollpandyaa <anmolpandya75@gmail.com>
2026-04-12 16:34:12 +0530

    removed syntax error in apiHandler

--- src/utils/apiErrorHandler.js
+++ src/utils/apiErrorHandler.js
@@ -5,7 +5,7 @@ class ApiError extends Error{
         this.data = null
         this.message = message
         this.success = false
-        this.errors = this.errors
+        this.errors = errors
 
         if (stack) this.stack = stack
         else Error.captureStackTrace(this, this.constructor)
