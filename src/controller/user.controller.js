2e75bfad0ff45ae52f39f15424d3f562316662a7
anmollpandyaa <anmolpandya75@gmail.com>
2026-04-25 17:39:08 +0530

    test user controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,9 +1,16 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/apiErrorHandler.js"
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    const {fullName, username, email, password} = req.body
+    console.log(fullName, username, email, password)
+
+    // check if any field is empty using some method
+    if ([fullName, username, email, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "all fields are compulsory")
+    }
+
+    
 })
 
 export {registerUser}
